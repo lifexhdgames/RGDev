12d690d82ec347e873448d5d9918529bf93b74ec
lifexhdgames <lifexhdgames@gmail.com>
2017-06-03 20:04:35 -0400

    Subida de archivos del proyecto RoyaleGroup

---
+++ base/index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>RoyaleGroup | Grupo Hispano</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
+		<link rel="stylesheet" href="css.css">
+		<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+	</head>
+
+	<body>
+		<header>
+			<div id="logo">RoyaleGroup</div>
+		</header>
+	</body>
+
+
+</html>
